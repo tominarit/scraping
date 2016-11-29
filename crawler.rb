0f281347176c5f53ed393b65d4160e13a7c438f2
tominari <tominari@hitokuse.com>
2016-11-29 15:56:27 +0900

    first draft

--- crawler.rb
+++ crawler.rb
@@ -1,12 +1,14 @@
-
 require 'open-uri'
 require 'Nokogiri'
+require 'mysql2'
+
+client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "shrewsbury12", :database => "yahoo_rss")
 
 # スクレイピング先のURL
 original_url = 'http://headlines.yahoo.co.jp/rss/list'
 
 #トピックスURLを格納する配列
-url = []
+links = []
 
 charset = nil
 html = open(original_url) do |f|
@@ -14,15 +16,34 @@ html = open(original_url) do |f|
   f.read # htmlを読み込んで変数htmlに渡す
 end
 
-# htmlをパース(解析)してオブジェクトを生成
+# htmlをパースしてオブジェクトを生成
 doc = Nokogiri::HTML.parse(html, nil, charset)
 
 #対象トピックスのURLを取得し配列に追加
 doc.css('a').each do |element|
-  #p element[:href]
   if element[:href].include?("http://news.yahoo.co.jp/pickup/")
-    url << element[:href]
+    links << element[:href]
   end
 end
 
-puts url
+#ここからXMLの処理
+
+#DBに格納するデータを入れる配列
+content = []
+
+links.each do |link|
+  xml = Nokogiri::XML(open(link).read)
+  item_nodes = xml.xpath('//channel/item')
+  item_nodes.each do |item|
+    title = item.xpath('title')
+    link = item.xpath('link')
+    pubDate = item.xpath('pubDate')
+    guid = item.xpath('guid')
+    client.query("insert into Yahoo_RSS (title) values ('#{title}')")
+    client.query("insert into Yahoo_RSS (link) values ('#{link}')")
+    client.query("insert into Yahoo_RSS (pubDate) values ('#{pubDate}')")
+    client.query("insert into Yahoo_RSS (guid) values ('#{guid}')")
+  end
+end
+
+#puts item.xpath('guid').text
