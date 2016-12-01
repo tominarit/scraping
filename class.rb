d7f54c733a10c534680b1be6e8df46a1a0188da0
tominari <tominari@hitokuse.com>
2016-12-01 17:01:36 +0900

    put the code into a class

--- class.rb
+++ class.rb
@@ -2,10 +2,11 @@ require 'open-uri'
 require 'Nokogiri'
 require 'mysql2'
 require 'csv'
-require '.const.rb'
+require './const'
 
 class YahooRSSGetter
   attr_accessor :original_url
+  attr_accessor :target_urls
 
   TARGET_URL = "http://news.yahoo.co.jp/pickup/"
 
@@ -23,30 +24,41 @@ class YahooRSSGetter
   def get_urls
     @target_urls = []
     doc = Nokogiri::HTML.parse(@html, nil, @charset)
+
     doc.css('a').each do |element|
       @target_urls << element[:href] if element[:href].include?(TARGET_URL)
     end
   end
 
-  def save_details
+  def import_data
+    #@client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "shrewsbury12", :database => "Yahoo_RSS")
     @client = Mysql2::Client.new(:host => HOST, :username => USERNAME, :password => PASS, :database => DB)
     target_urls.each do |url|
-      xml = Nokogiri::XML(open(link).read)
+      xml = Nokogiri::XML(open(url).read)
       item_nodes = xml.xpath('//channel/item')
-
       item_nodes.each do |item|
         @title = item.xpath('title').text
         @link = item.xpath('link').text
         @pubDate = item.xpath('pubDate').text
         @guid = item.xpath('guid').text
 
-        client.query("insert into Yahoo_RSS (title, link, pubDate, guid) values ('#{title}', '#{link}', '#{pubDate}', '#{guid}')")
+        @client.query("insert into Yahoo_RSS (title, link, pubDate, guid) values ('#{@title}', '#{@link}', '#{@pubDate}', '#{@guid}')")
       end
     end
   end
+
+  def export_data
+    if File.exist?("result.csv")
+      @client.query("select * into outfile '/Users/tominarit/Desktop/ruby_class/result#{Date.today}.csv' fields terminated by ',' from Yahoo_RSS")
+    else
+      @client.query("select * into outfile '/Users/tominarit/Desktop/ruby_class/result.csv' fields terminated by ',' from Yahoo_RSS")
+    end
+  end
+
 end
 
-web = YahooRSSGetter.new("http://headlines.yahoo.co.jp/rss/list")
-web.get_html
-web.get_urls
-web.save_details()
+yahoorss = YahooRSSGetter.new("http://headlines.yahoo.co.jp/rss/list")
+yahoorss.get_html
+yahoorss.get_urls
+yahoorss.import_data
+yahoorss.export_data
